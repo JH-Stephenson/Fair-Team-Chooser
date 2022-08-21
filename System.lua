a5cfcc41356389c22ae3f7e13866a13206ee1e6b
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-08-21 12:39:43 +0100

    Created Initial System for Choosing Players

--- System.lua
+++ System.lua
@@ -5,5 +5,61 @@ local Players = game:GetService("Players")
 local Teams = game:GetService("Teams")
 
 --ServiceVariables
-local Team1 = Teams.Team1
-local Team2 = Teams.Team2
+local Team1 = Teams.Team1 -- Change For Your Teams Here
+local Team2 = Teams.Team2 -- Change For Your Teams Here
+
+--SystemTables
+local PlayerTable = {}
+local LastChosenTable = {}
+
+--SystemVariables
+local SystemCounter = 0
+
+--LocalFunctions
+local function ChooseTeams()
+    --ChooseRandomly
+    local ChoicePicker = math.random(1, #PlayerTable)
+
+    --FunctionVariables
+    local ConfirmedPlayer = false
+
+    for _, Player in LastChosenTable do
+        if Player == PlayerTable[ChoicePicker] then
+            ConfirmedPlayer = true
+        end
+    end
+
+    if ConfirmedPlayer == true then
+        ChooseTeams()
+    else
+        SystemCounter = SystemCounter + 1
+        table.insert(LastChosenTable, PlayerTable[ChoicePicker])
+        
+        for _, ServicePlayer in Players:GetChildren() do
+            if ServicePlayer.UserId == PlayerTable[ChoicePicker] then
+                ServicePlayer.Team = Team2
+            end
+        end
+
+        if SystemCounter == #PlayerTable then
+            SystemCounter = 0
+            
+            for _, Position in LastChosenTable do
+                table.remove(LastChosenTable, Position)
+            end
+        end
+    end
+end
+
+--SystemEvents
+Players.PlayerAdded:Connect(function(Player)
+    table.insert(PlayerTable, Player.UserId) -- Add Player To Table
+end)
+
+Players.PlayerRemoving:Connect(function(RemovedPlayer)
+    for _, Player in PlayerTable do
+        if Player == RemovedPlayer.UserId then
+            table.remove(PlayerTable, Player) -- Remove Player From System
+        end
+    end
+end)
