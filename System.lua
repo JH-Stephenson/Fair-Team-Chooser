251fe1a62d589ff4e570168e001a4fbcbd27cb35
James. H. Stephenson <77564249+JH-Stephenson@users.noreply.github.com>
2022-08-21 11:40:48 +0100

    Created Services and Team Variables

---
+++ System.lua
@@ -0,0 +1,9 @@
+--TeamChoosingSystem
+
+--Services
+local Players = game:GetService("Players")
+local Teams = game:GetService("Teams")
+
+--ServiceVariables
+local Team1 = Teams.Team1
+local Team2 = Teams.Team2
